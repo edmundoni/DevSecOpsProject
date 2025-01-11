fb210c71d7198048adef4e386d94b46652adea00
Edmundoni <edmund.oni@gmail.com>
2025-01-11 11:56:11 +0000

    update

--- main.tf
+++ main.tf
@@ -1,6 +1,5 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraform-nov-project23" {
-  bucket = "terraform-nov-project23"
-  }
+resource "aws_s3_bucket" "terraform-falconwings-dev" {
+}
