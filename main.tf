70d7e21825e5043f2558ea153bc83148ccaee3c3
Edmundoni <edmund.oni@gmail.com>
2025-01-18 20:25:20 +0000

    update

--- main.tf
+++ main.tf
@@ -1,5 +1,14 @@
 provider "aws" {
   region = "eu-west-2" # London region
 }
-resource "aws_s3_bucket" "terraform-falconwings-dev" {
+resource "aws_s3_bucket" "terraform-project001" {
+  bucket = "terraform-project001"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraform-project001"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
 }
